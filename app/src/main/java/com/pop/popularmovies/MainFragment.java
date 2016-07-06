a401a42921e374e0221f875ea29b7fb90d2d2957
poph2 <niyipopp@yahoo.co.uk>
2016-07-06 18:04:40 +0100

    Extracted information from JSON

--- app/src/main/java/com/pop/popularmovies/MainFragment.java
+++ app/src/main/java/com/pop/popularmovies/MainFragment.java
@@ -3,13 +3,20 @@ package com.pop.popularmovies;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
+import com.pop.popularmovies.util.APIGetter;
+import com.pop.popularmovies.util.ImageLoader;
 import com.pop.popularmovies.util.MovieItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -38,7 +45,7 @@ public class MainFragment extends Fragment {
         movieItemList.add(new MovieItem("Bittersweet",  R.drawable.bittersweet));
         movieItemList.add(new MovieItem("Deep Lilac",   R.drawable.deep_lilac));
         movieItemList.add(new MovieItem("Gold",         R.drawable.gold));
-        movieItemList.add(new MovieItem("Azure",        R.drawable.azure));
+        //movieItemList.add(new MovieItem("Azure",        R.drawable.azure));
 
         mMovieAdapter = new MovieGridAdapter(
                 getActivity(),
@@ -54,6 +61,7 @@ public class MainFragment extends Fragment {
         GridView gridView = (GridView) rootView.findViewById(R.id.movie_gridview);
         gridView.setAdapter(mMovieAdapter);
 
+        updateMovies();
 
         // Inflate the layout for this fragment
         return rootView;
@@ -66,36 +74,79 @@ public class MainFragment extends Fragment {
     }
 
     public void updateMovies() {
+        //Toast.makeText(getContext(), "HERE1111", Toast.LENGTH_LONG).show();
         MovieAsyncTask movieAsyncTask = new MovieAsyncTask();
         movieAsyncTask.execute();
     }
 
+    private void loadImage() {
+        imageLoader = new ImageLoader(MainActivity.this);
+
+        Holder holder = new Holder();
+        holder.ivPhoto = (ImageView) findViewById(R.id.imageView);
+        imageLoader.DisplayImage(clipboardImageUrl, holder.ivPhoto);
+
+        textView.setText(clipboardUsername);
+
+        btnShare.setEnabled(true);
+
+        pd.dismiss();
+    }
+
     class MovieAsyncTask extends AsyncTask<Void, Void, ArrayList<MovieItem>> {
+
+        private final String LOG_TAG = MovieAsyncTask.class.getSimpleName();
 
         @Override
         protected ArrayList<MovieItem> doInBackground(Void... params) {
+
+            String movieJson = APIGetter.doAction2();
 
             ArrayList<MovieItem> movieItemList = new ArrayList<>();
 
             movieItemList.add(new MovieItem("Red",          R.drawable.red));
             movieItemList.add(new MovieItem("Aquamarine",   R.drawable.aquamarine));
             movieItemList.add(new MovieItem("Bittersweet",  R.drawable.bittersweet));
-            movieItemList.add(new MovieItem("Deep Lilac",   R.drawable.deep_lilac));
-            movieItemList.add(new MovieItem("Gold",         R.drawable.gold));
-            movieItemList.add(new MovieItem("Azure",        R.drawable.azure));
+            //movieItemList.add(new MovieItem("Deep Lilac",   R.drawable.deep_lilac));
+            //movieItemList.add(new MovieItem("Gold",         R.drawable.gold));
+            //movieItemList.add(new MovieItem("Azure",        R.drawable.azure));
 
-
+            try {
+                movieItemList.remove(0);
+                movieItemList = getMovieItemsFromJson(movieJson);
+                //Toast.makeText(getActivity(), "HERE", Toast.LENGTH_LONG).show();
+            }
+            catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
 
             return movieItemList;
+        }
+
+        public ArrayList<MovieItem> getMovieItemsFromJson(String jsonString) throws JSONException {
+            ArrayList<MovieItem> movieItemList = new ArrayList<>();
+
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray resultJsonArray = jsonObject.getJSONArray("results");
+
+            for(int i = 0; i < resultJsonArray.length(); i++) {
+                JSONObject movieJsonObject = resultJsonArray.getJSONObject(i);
+                movieItemList.add(new MovieItem(movieJsonObject.getString("original_title"), R.drawable.red));
+            }
+
+            return movieItemList;
+
         }
 
         @Override
         protected void onPostExecute(ArrayList<MovieItem> movieItemList) {
             if (movieItemList != null) {
                 mMovieAdapter.clear();
-                mMovieAdapter.addAll(movieItemList);
+                for(MovieItem movieItem : movieItemList) {
+                    mMovieAdapter.add(movieItem);
+                }
             }
-            Toast.makeText(getActivity(), "RED", Toast.LENGTH_SHORT).show();
         }
     }
 
